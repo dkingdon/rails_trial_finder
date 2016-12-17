1c01c9f357bb2ecb5a09aaf8822a8ef73eba2d2a
dkingdon <daniel.kingdon@gmail.com>
2016-12-16 22:23:35 -0800

    map appearing on page, next steps to hook markers up based on which trails are displaying.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,20 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
 //= require jquery
 //= require bootstrap-sprockets
+//= require underscore
+//= require gmaps_google
+//= require_tree .
+
+console.log("app.js is connected.");
+
+
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 37.78, lng: -122.44},
+  zoom: 8
+ });
+}
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,13 @@
 class WelcomeController < ApplicationController
   def index
     @trails = Trail.all
+    # @hash = Gmaps4rails.build_markers(@trails) do |trail, marker|
+    #   marker.lat trail.lat
+    #   marker.lng trail.lng
+    # end
     # render :index
   end
 
-# hide trails, have buttons change value of variable. variable value deturmines which trails. 
+# hide trails, have buttons change value of variable. variable value deturmines which trails.
 
 end
